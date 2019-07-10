7e9aac9df12e5320de67c3afded502877c98c568
tavisca-vitripathi <tripathi.t@yahoo.com>
2019-07-10 19:41:28 +0530

    Update Program.cs

--- Tavisca.Bootcamp.LanguageBasics.Exercise3/Program.cs
+++ Tavisca.Bootcamp.LanguageBasics.Exercise3/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+    
+using System;
 using System.Linq;
 
 namespace Tavisca.Bootcamp.LanguageBasics.Exercise1
@@ -41,7 +42,112 @@ namespace Tavisca.Bootcamp.LanguageBasics.Exercise1
         public static int[] SelectMeals(int[] protein, int[] carbs, int[] fat, string[] dietPlans)
         {
             // Add your code here.
-            throw new NotImplementedException();
+              int[] calorie = new int[protein.Length];
+            for(int i = 0; i < protein.Length; i++)
+            {
+                calorie[i] = fat[i] * 9 + protein[i] * 5 + carbs[i] * 5;
+
+            }
+             int[] exp = new int[dietPlans.Length];
+            for(int i = 0; i < dietPlans.Length; i++)
+            {
+                List<int> allowed = new List<Int32>();
+                char[] plans = dietPlans[i].ToCharArray();
+                for (int k = 0; k < protein.Length; k++)
+                {
+                    allowed.Add(k);
+
+                }
+                for (int j = 0; j < plans.Length; j++)
+                {
+                    if (plans[j].Equals('P'))
+                    {
+                        int max = getValue(protein, allowed);
+                        allowed = getAllowed(protein, allowed, max);
+                        
+                    }
+                    else if (plans[j].Equals('p'))
+                    {
+                        int min = getValue(protein, allowed,"min");
+                        allowed = getAllowed(protein, allowed, min);
+                    }
+                    else if (plans[j].Equals('C'))
+                    {
+                        int max = getValue(carbs, allowed);
+                        allowed = getAllowed(carbs, allowed, max);
+                    }
+                    else if (plans[j].Equals('c'))
+                    {
+                        int min = getValue(carbs, allowed,"min");
+                        allowed = getAllowed(carbs, allowed,min);
+                    }
+                    else if (plans[j].Equals('F'))
+                    {
+                        int max = getValue(fat, allowed);
+                        allowed = getAllowed(fat, allowed, max);
+                    }
+                    else if (plans[j].Equals('f'))
+                    {
+                        int min = getValue(fat, allowed,"min");
+                        allowed = getAllowed(fat, allowed, min);
+                    }
+                    else if (plans[j].Equals('T'))
+                    {
+                        int max = getValue(calorie, allowed);
+                        allowed = getAllowed(calorie, allowed, max);
+                    }
+                    else if (plans[j].Equals('t'))
+                    {
+                        int min = getValue(calorie, allowed,"min");
+                        allowed = getAllowed(calorie, allowed, min);
+                    }
+                }
+                exp[i] = allowed[0];
+            }
+                        return exp;
+        }
+
+        public static int getValue(int[] arr,List<int> allowed,string op = "max")
+        {
+            int min = int.MaxValue;
+            int max = int.MinValue;
+            if (op.Equals("max"))
+            {
+                for(int i = 0; i < allowed.Count; i++)
+                {
+                    if (max < arr[allowed[i]])
+                    {
+                        max = arr[allowed[i]];
+                    }
+                }
+                return max;
+            }
+            else
+            {
+                for (int i = 0; i < allowed.Count; i++)
+                {
+                    if (min > arr[allowed[i]])
+                    {
+                        min = arr[allowed[i]];
+                    }
+                }
+                return min;
+            }
+            
+        }
+
+        public static List<int> getAllowed(int[] arr,List<int> allowed,int val)
+        {
+            List<int> tempAllowed = new List<int>();
+            for(int i = 0; i < allowed.Count; i++)
+            {
+                if (arr[allowed[i]] == val)
+                {
+                    tempAllowed.Add(allowed[i]);
+                }
+            }
+            return tempAllowed;
+            //throw new NotImplementedException();
         }
     }
 }
